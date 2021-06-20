1bc71914356057cd7dd2e44fc04ebb86d56df22a
Nurlan <n.satyvaldiev@e-sot.kg>
2021-06-20 19:52:27 +0600

    second commit

--- blog_kg/settings.py
+++ blog_kg/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
